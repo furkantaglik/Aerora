c979099f721f816f535ff8841dcbefbb1b5df8dc
furkan <132236699+furkantaglikk@users.noreply.github.com>
2023-09-09 20:23:39 +0300

    ilk Commit

--- helpers/ApiHelpers.js
+++ helpers/ApiHelpers.js
@@ -2,7 +2,7 @@ const key = "d04f8dbcbdee84f5c7d7c28cfce834a0"
 
 export async function geoCoding(city) {
     const cityName = city ?? "Istanbul";
-    return await (await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${key}`)).json()
+    return await (await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${key}`)).json()
 }
 
 export async function currentWeatherApi(lat, lon) {
