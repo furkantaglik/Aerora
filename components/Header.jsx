001cd42aaeea4a4a8f1316b0696d6122ce52af3c
furkan <132236699+furkantaglikk@users.noreply.github.com>
2023-09-09 20:25:58 +0300

    ilk Commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import Link from "next/link";
 
 const Header = () => {
     return (
         <header className='flex items-center justify-center p-5 bg-black rounded-b-full mb-10'>
             <div>
-                <h1 className='text-3xl text-teal-500 font-extrabold'>Aerora</h1>
+                <Link href="/" className='text-3xl text-teal-500 font-extrabold'>Aerora</Link>
             </div>
         </header>
     )
