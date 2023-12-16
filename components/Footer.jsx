021acdf2075f1a0d83bc253ca4875fb14602658b
Furkan <132236699+furkantaglik@users.noreply.github.com>
2023-12-16 16:30:42 +0300

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className='flex items-center justify-center bg-black p-10 mt-10'>
+    <footer className='flex items-center justify-center  p-10 mt-10'>
         <div>
            
             <p className='text-white'>Created by <Link href="https://github.com/furkantaglik" className='text-teal-700 font-bold hover:text-teal-500'>Furkan Tağlık</Link></p>
